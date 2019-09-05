dd9fa7db5ac8ff38b352a00c3921daeb1b7e93ad
Phils Garcia <eivanphils@gmail.com>
2019-09-05 18:25:05 -0400

    eliminada pagina no utlizada y actualizada las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,12 +12,12 @@ const routes: Routes = [
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
   },
   {
-    path: 'list',
-    loadChildren: () => import('./pages/list/list.module').then(m => m.ListPageModule)
-  },
-  {
     path: 'login',
     loadChildren: () => import ('./pages/login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
   }
 ];
 
